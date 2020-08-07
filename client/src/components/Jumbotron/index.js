f6c918f322cfa3c073dd205ead518ad6facfe237
Lance Page <6ftdotexe@users.noreply.github.com>
2020-08-06 20:57:58 -0500

    tested skeleton with search function and basic UI

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Fridge Buddy</h1>
+    </div>
+  );
+}
+
+export default Jumbotron;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-primary bg-dark">
+      <a className="navbar-brand" href="/">
+        Fridge Buddy
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
