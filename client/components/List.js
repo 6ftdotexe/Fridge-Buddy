7580ed306613723952e9e23fa6bf6833c559d6ae
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-08-13 20:29:15 -0500

    Adding the GroceryList Function

---
+++ client/components/List.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+class GroceryList extends Component {
+    constructor(props) {
+        super(props);
+        // Setting the component's initial state
+        this.state = {
+            groceryItems: [],
+            currentItem: ""
+        };
+    }
+
+    handleInputChange = event => {
+        this.setState({
+            currentItem: event.target.value
+        });
+
+    }
+    addItem = () => {
+        this.setState(state => {
+            const groceryItems = state.groceryItems.concat(state.currentItem);
+            return {
+                groceryItems,
+                currentItem: '',
+            };
+        })
+    }
+    removeItem = (index) => {
+
+        let newList = [...this.state.groceryItems];
+        newList.splice(index, 1)
+        console.log(newList);
+        console.log(index);
+        this.setState({
+            groceryItems: newList
+        })
+
+    }
+    handleFormSubmit = () => {
+        this.addItem();
+    }
+
+    render() {
+
+        return (
+
+
+
+            <div>
+
+                <div>
+                    {this.state.groceryItems.map((item, index) => (
+                        <li key={item} >
+                            <input type="checkbox" />{item}
+
+                            <button onClick={() => this.removeItem(index)}>X</button>
+                        </li>
+                    )
+                    )}
+                    <input
+                        value={this.state.currentItem}
+                        onChange={this.handleInputChange}
+                        type="text"
+                        placeholder="Add to list"
+                    />
+
+                    <button onClick={this.handleFormSubmit}>Submit</button>
+                </div>
+            </div >
+
+
+        );
+    }
+}
+
+
+
+export default GroceryList;
