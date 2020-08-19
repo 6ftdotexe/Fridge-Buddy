954864c61f09fb8ee335e64255eb030265c5d711
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-08-18 20:31:27 -0500

    Update Recipe.js
    Updating and adding to the singled out Recipe Page.

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Card, CardTitle, CardBody, CardHeader, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 const APP_ID = '4fd7f07d';
 const APP_KEY = '6366fb53422c0710914acf9000b5c1d6';
 
@@ -12,30 +12,49 @@ class Recipe extends Component {
 
         const title = this.props.location.state.recipe;
 
-
-
         const req = await fetch(`https://api.edamam.com/search?q=${title}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const res = await req.json();
 
-        // and since e is an object means  we can access the 'properties' of the 'object'.
-        const data = await req.json();
-        // Set the state 'recipes' to the api call
-        this.setState({ activeRecipe: data.recipe.label })
-        // console.log(this.state.recipes)
+        console.log("--------------------------------------");
+        console.log(res.hits);
+        //     // and since e is an object means  we can access the 'properties' of the 'object'.
+
+        //     // Set the state 'recipes' to the api call
+        this.setState({ activeRecipe: res.hits[0].recipe })
+        console.log("--------------------------------------");
+        console.log('"The Active Recipe is: " ')
+        console.log(this.state.activeRecipe);
+        //     // console.log(this.state.recipes)
 
     }
+
+
+
     render(props) {
+        const recipe = this.state.activeRecipe;
+
         // console.log(this.props)
+
         return (
             <>
-                <Container>
-                    <Card>
-                        <CardBody>
+                <div className="container">
+                    {/* //Once you have filled the active recipe state then display the page. */}
+                    {this.state.activeRecipe.length !== 0 &&
+                        <>
+                            <h2>{recipe.label} by {recipe.source}</h2>
+                            <a href={recipe.url} ><h2>Recipe found here</h2></a>
+                            <h3>Cook Time: {recipe.totalTime}</h3>
+                            <img className="active__recipe" src={recipe.image} ></img>
 
-                        </CardBody>
+                            <button>
+                                <Link to="/">
+                                    Go back
+                                </Link>
+                            </button>
+                        </>
+                    }
 
-                    </Card>
-
-                </Container>
+                </div>
             </>
         )
     }
